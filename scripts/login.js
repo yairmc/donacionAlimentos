75321b00213d8c331f46bf3f554b8e4714ba55d9
yairmc <yairmc666@gmail.com>
2022-11-27 12:34:19 -0700

    changes in the signUp

--- scripts/login.js
+++ scripts/login.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const addListeners = () => {
     loginForm.addEventListener("submit", fetchLogin);
-    // signUpForm.addEventListener("submit", fetchSignUp);
 }
 
 const fetchLogin = async (e) => {
@@ -48,48 +47,3 @@ const fetchLogin = async (e) => {
 
     }
 }
-
-// SignUp 
-
-// const fetchSignUp = async (e) => {
-//     e.preventDefault();
-//     const newUsername = document.querySelector('#newUsername');
-//     const newPassword = document.querySelector('#newPassword');
-//     const repeatPassword = document.querySelector('#repeatPassword');
-
-//     if (newUsername.value === '' || newPassword.value === '' || repeatPassword.value === '') {
-//         alert('Porfavor llenar todos los campos');
-//         newUsername.value = '';
-//         newPassword.value = '';
-//         repeatPassword.value = '';
-//         return;
-//     } else {
-
-//         try {
-//             const credentials = {
-//                 username: newUsername.value,
-//                 password: newPassword.value
-//             }
-
-//             const response = await fetch('https://fcback-production.up.railway.app/signUp', {
-//                 method: 'POST',
-//                 headers: { 'Content-Type': 'application/json' },
-//                 body: JSON.stringify(credentials)
-//             });
-//             const data = await response.json();
-//             if (data === 'signUp success') location.href = "/pages/menuPrincipal.html"
-//             else {
-//                 alert(data);
-//                 location.reload();
-//             }
-//         } catch (error) {
-//             console.log(error);
-//         }
-//         newUsername.value = '';
-//         newPassword.value = '';
-//         repeatPassword.value = '';
-//     }
-// }
-
-
-// 5432 port of possgres 
---
+++ scripts/signUp.js
@@ -0,0 +1,85 @@
+
+// Loggin 
+let signUpForm = document.querySelector('#signUpForm');
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    addListeners();
+})
+
+const addListeners = () => {
+    signUpForm.addEventListener("submit", fetchSignUp);
+}
+// SignUp 
+
+const fetchSignUp = async (e) => {
+    e.preventDefault();
+    const donador = document.querySelector('#donador')
+    const centro = document.querySelector('#centro')
+    const newUsername = document.querySelector('#newUsername');
+    const newPassword = document.querySelector('#newPassword');
+    const repeatPassword = document.querySelector('#repeatPassword');
+    const address = document.querySelector('#address');
+    const celphone = document.querySelector('#celphone');
+    const timeStart = document.querySelector('#timeStart');
+    const timeEnd = document.querySelector('#timeEnd');
+
+    let typeUser;
+    if (donador.checked) typeUser = 'donador'
+    else if (centro.checked) typeUser = 'centro'
+    else alert('Escoge un usuario')
+
+    if (newUsername.value === '' || newPassword.value === ''
+        || repeatPassword.value === '' || address.value === '' || celphone.value === '') {
+        alert('Porfavor llenar todos los campos');
+        donador.checked = false;
+        centro.checked = false;
+        newUsername.value = '';
+        newPassword.value = '';
+        repeatPassword.value = '';
+        address.value = ''
+        celphone.value = '';
+        timeStart.value = '';
+        timeEnd.value = '';
+        return;
+    } else {
+
+        try {
+            const credentials = {
+                user: typeUser,
+                username: newUsername.value,
+                password: newPassword.value,
+                address: address.value,
+                celphone: celphone.value,
+                timeStart: timeStart.value,
+                timeEnd: timeEnd.value
+            }
+
+            const response = await fetch('https://fcback-production.up.railway.app/signUp', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(credentials)
+            });
+            const data = await response.json();
+            if (data === 'signUp success') location.href = "/pages/login.html"
+            else {
+                alert(data);
+                location.reload();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        donador.checked = false;
+        centro.checked = false;
+        newUsername.value = '';
+        newPassword.value = '';
+        repeatPassword.value = '';
+        address.value = ''
+        celphone.value = '';
+        timeStart.value = '';
+        timeEnd.value = '';
+    }
+}
+
+
+// 5432 port of possgres 
